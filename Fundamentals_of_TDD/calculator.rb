2bd7fc797f245900778b6c21b6d311f51e29dee2
Oscar Romero <creaturenex@gmail.com>
2022-09-09 20:21:58 -0400

    added first test

--- Fundamentals_of_TDD/calculator.rb
+++ Fundamentals_of_TDD/calculator.rb
@@ -2,3 +2,14 @@ require "rspec/autorun"
 
 # calc = Calculator.new
 # calc.add(2, 10) # => 15
+
+
+describe Calculator do
+    describe "#add" do
+        it "returns the sum of its two arguments" do
+            calc = Calculator.new
+            
+            expect(calc.add(5, 10).to eq(15))
+        end
+    end
+end
